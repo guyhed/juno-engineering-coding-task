e5156b4da320a23384e633edb25d88b4d67ef5ed
Guy Hed <guyhe@cet.ac.il>
2022-10-04 02:15:29 +0300

    adding a note and padding to the app

--- src/task1/App.tsx
+++ src/task1/App.tsx
@@ -2,11 +2,15 @@ import logo from '../logo.svg';
 import ImageCarousel from "./ImageCarousel";
 
 function App() {
-  return (<div>
-    <div>Here is an image carousel:</div>
+  return (<div style={{padding:'20px 50px'}}>
+    <h3>Here is an image carousel:</h3>
     <div>
       <ImageCarousel />
     </div>
+    <p>Note: when the cache is disabled, the images are loaded slowly in the carousel, 
+      even after they were fetched. There are way are ways around it 
+      (e.g. painting the fetched image on a canvas) but since most users do not
+      browse with their devtools open, I let it go.</p>
   </div>);
 }
 
